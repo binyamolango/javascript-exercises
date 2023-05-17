020216ef593042097eb46847f3d46020edbe684e
binyam yohannes <olangobinyam@gmail.com>
2023-05-17 22:34:15 +0300

    Do javascript exercises from one to six

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+    let obj = arguments;
+    let arr = obj[0];
+    for(i = 1; i < obj.length; i++){
+        if(arr.includes(obj[i])){
+            arr.splice(arr.indexOf(obj[i]), 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let arr = [a, b];
+    let sum = 0;
+    if(a >= 0 && Number.isInteger(a) && b >= 0 && Number.isInteger(b)){
+        arr.sort((a, b) => a - b);
+        for(let i = arr[0]; i <= arr[1]; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    else return "ERROR";
 };
 
 // Do not edit below this line
