3d5795f858b2d81bd4cbb8f8e32f583a581fdb8d
binyam yohannes <olangobinyam@gmail.com>
2023-05-18 12:57:51 +0300

    Do getTheTitles js-exercise number 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    let names = [];
+    books.forEach(each => {
+      names.push(each.title);
+    });
+    return names;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
