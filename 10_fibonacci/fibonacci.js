728ad8393703808ac215f51b5423c55eb664a4ac
binyam yohannes <olangobinyam@gmail.com>
2023-05-18 12:32:36 +0300

    Do fibonacci javascript exercise no 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,36 @@
-const fibonacci = function() {
-
+const fibonacci = function(k) {
+    let arr = [0, 1];
+    if(Number.isInteger(k)){
+        if(k < 0){
+            return "OOPS";
+        }
+        else if(k === 0){
+            return 0;
+        }
+        for(let i = 0; ; i++){
+            let x = arr[i] + arr[i+1];
+            arr.push(x);
+            if(arr.length - 1 === k){
+                return arr[k];
+            }
+        }
+    }
+    else if(!isNaN(k)){
+        let newK = parseInt(k);
+        if(newK < 0){
+            return "OOPS";
+        }
+        else if(newK === 0){
+            return 0;
+        }
+        for(let i = 0; ; i++){
+            let x = arr[i] + arr[i+1];
+            arr.push(x);
+            if(arr.length - 1 === newK){
+                return arr[newK];
+            }
+        }
+    }
 };
 
 // Do not edit below this line
