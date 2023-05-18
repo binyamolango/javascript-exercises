bfb9abada6b4c7dadaf79b26efdb468833082786
binyam yohannes <olangobinyam@gmail.com>
2023-05-18 09:02:04 +0300

    Work palindromes js-exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let regex = /[^a-z|0-9]/g;
+    let newStr = str.toLowerCase().replace(regex, "");
+    if(newStr === newStr.split("").reverse().join("")){
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
